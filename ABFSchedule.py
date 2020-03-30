683258ceb92e1cf90a68e6e4087c0455253d0155
ClevelandFan295 <52326290+ClevelandFan295@users.noreply.github.com>
2020-03-29 21:42:07 -0400

    hm:

--- ABFSchedule.py
+++ ABFSchedule.py
@@ -9,7 +9,7 @@ Generate a schedule as described by the suggestion on 08/15/19
 ------------------------------------------
 CHANGE FILENAME AND DIVISION MATCHUPS HERE
 '''
-filename = 'currentExport3.json'
+filename = 'currentExport4.json'
 alignment = 3 #determines which divisions play each other (1, 2, or 3) - card
 '''
 ------------------------------------------
